15e048be93efcd0116de44c34074c94a04c1145d
RafaelSeidi <rafasuzukitomita@gmail.com>
2024-10-20 18:10:37 -0300

    primeiro commit]

---
+++ src/main/java/br/com/fiap/ecommerce/dtos/ProdutoRequestCreateDto.java
@@ -0,0 +1,11 @@
+package br.com.fiap.ecommerce.dtos;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class ProdutoRequestCreateDto {
+    private String nome;
+    private BigDecimal valor;
+}
---
+++ src/main/java/br/com/fiap/ecommerce/dtos/ProdutoRequestUpdateDto.java
@@ -0,0 +1,11 @@
+package br.com.fiap.ecommerce.dtos;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class ProdutoRequestUpdateDto {
+	private String nome;
+	private BigDecimal valor;
+}
